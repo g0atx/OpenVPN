1d72af9401d752692d48925fb83f24e1150ec51e
g0atx <49911423+g0atx@users.noreply.github.com>
2022-06-08 18:09:32 -0500

    Add files via upload

---
+++ OpenVPN.ps1
@@ -0,0 +1,17 @@
+﻿
+# This is a powershell script to automate setting up OpenVPN Server on Windows 10/11
+
+
+# Set Execution Policy if you encounter script running error
+# Get-ExecutionPolicy -List
+# Set-ExecutionPolicy RemoteSigned
+
+
+# Creates a directory in the root drive
+
+mkdir C:\OpenVPN
+
+# Downloads the OpenVPN file
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.7-I602-amd64.msi" -OutFile "C:\OpenVPN\OpenVPN.msi"
