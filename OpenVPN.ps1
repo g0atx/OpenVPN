b44f01d321ea57ae19fbfe1e384c7f248c55aa1a
g0atx <49911423+g0atx@users.noreply.github.com>
2022-06-08 18:10:24 -0500

    Update OpenVPN.ps1

--- OpenVPN.ps1
+++ OpenVPN.ps1
@@ -1,6 +1,4 @@
-﻿
 # This is a powershell script to automate setting up OpenVPN Server on Windows 10/11
-
 
 # Set Execution Policy if you encounter script running error
 # Get-ExecutionPolicy -List
@@ -8,10 +6,8 @@
 
 
 # Creates a directory in the root drive
-
 mkdir C:\OpenVPN
 
 # Downloads the OpenVPN file
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri "https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.7-I602-amd64.msi" -OutFile "C:\OpenVPN\OpenVPN.msi"
